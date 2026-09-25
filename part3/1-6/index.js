b8652f18d18e8e8c6656018ec30df6bb349cb7b0
Sean <spmandrick@gmail.com>
2026-09-25 10:30:01 -0400

    Fixing PORT

--- part3/1-6/index.js
+++ part3/1-6/index.js
@@ -127,7 +127,7 @@ app.get('/info', (request, response) => {
     response.send(infoString)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
